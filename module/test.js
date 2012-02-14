2a6f3892120d3e331b1c1800d80b0e0a1498d5bd
tuvd08 <duytucntt@gmail.com>
2012-02-15 00:56:48 +0700

    Issue 02

--- module/test.js
+++ module/test.js
@@ -1,6 +1,11 @@
 
+function alertct(info) {
+	var elm = document.getElementById("info");
+	elm.innerHTML += "<br/>" + info;
+	elm.className = "border";
+} 
  // test case module
- 
+ // S-01
 (function() {
 	   
     var f = window[F_NAME],
@@ -10,11 +15,46 @@
         currentDocument = currentWindow.document;
 		
     var publicity = {
-        createTestCase: function(name) {
-            alert(name);
+        createTestCaseS1: function(name) {
+        	alertct(name);
         }
-    }
+    };
        
+	f(publicity).addTo("utility");
+
+})();
+
+// S-02
+
+(function() {
+	   
+    var f = window[F_NAME],
+        copy = f.callFunction('copy'),
+        assert = f.callFunction('assert'),
+        currentWindow = f.getWindow(),
+        currentDocument = currentWindow.document;
+		
+    var publicity = {
+		createTestCase : function(name, obj) {
+			alertct("Run test case name: " + name);
+			return {
+				"case1" : function() {
+					alertct("case 1");
+				},
+				"case2" : function() {
+					alertct("case 2");
+				},
+				"case3" : function() {
+					alertct("case 3");
+				}
+			};
+		},
+		testCase : function(obj) {
+			obj.case1();
+			obj.case2();
+			obj.case3();
+		}
+	};
 	f(publicity).addTo("utility");
 
 })();
