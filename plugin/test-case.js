c4acc50daf4d855a4d640299444b893f7e13bf49
Minh Nguyen <rieucua@gmail.com>
2012-05-08 00:16:38 +0700

    update test case plugin

--- plugin/test-case.js
+++ plugin/test-case.js
@@ -3,11 +3,7 @@
  
 (function() {
 	   
-    var f = window[F_NAME],
-        copy = f.callFunction('copy'),
-        assert = f.callFunction('assert'),
-        currentWindow = f.getWindow(),
-        currentDocument = currentWindow.document;
+    var f = window[F_NAME];
 		
     var publicity = {
         
@@ -18,31 +14,31 @@
                 var report = {
                     testName : testName || ""
                 };
-                
+                var test, start, passed, resultTest;
+				
                 for (var i in cases) {
-                    
                     if (!cases.hasOwnProperty(i)) continue;
-                    var test = {}, start = 0;
                     f.clearNotify();
+					test = [];
                     report[i] = test;
                     
                     try {
                         start = new Date().getTime();
                         cases[i]();
+						test.result = "success";
                     } catch(e) {
                         test.result = "test fails because got error.";
                     }
                     
                     test.duration = new Date().getTime() - start;
-                    var resultTest = f.getNotifyMessage();
-                    var passed = 0;
+                    resultTest = f.getNotifyMessage();
+                    passed = 0;
                     test.detail = {};
                     for (var o = 0; o < resultTest.length; ++o) {
                         if (resultTest[o].result == "pass") passed += 1;
                         test.detail[o] = resultTest[o];
                     }
-                   
-                    test.result = "success";
+                    
                     test.total = resultTest.length;
                     test.passed = passed;
                     test.failed = test.total - test.passed;
